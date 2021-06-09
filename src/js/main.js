7636f0b1f87150836524e4a21fa4584418b54e99
Hassan <hassan.alirobleh@gmail.com>
2021-06-09 17:54:40 +0200

    scroll sections active link

--- src/js/main.js
+++ src/js/main.js
@@ -134,14 +134,39 @@ let swiperTestimonial = new Swiper(".testimonial__container ", {
   breakpoints: {
     568: {
       slidesPerview: 2,
-    }
-  }
-
+    },
+  },
 });
 /* CONTACT ME */
 /* FOOTER */
 
 /* SCROLL SECTIONS ACTIVE LINK */
+const sections = document.querySelectorAll("section[id]");
+console.log(sections);
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
+
 /* CHANGE BACKGROUND HEADER */
+
 /* SHOW SCROLL TOP */
+
 /* DARK LIGHT THEME */
